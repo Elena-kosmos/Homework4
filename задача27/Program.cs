8aa47454a115594c85bedd4943799f42211aab72
Lena <eutereshkova@mail.ru>
2022-09-15 15:51:46 +0300

    добавили задачу 27

---
+++ задача27/Program.cs
@@ -0,0 +1,18 @@
+﻿// написать программу, которая принимает на вход число
+// и выдает сумму цифр в числе
+//425 -> 11
+Console.Write("Ведите число: ");
+int number= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumDigit(number));
+int SumDigit(int num)
+{   int sum =0;
+    while(num>0)
+{
+    sum += num%10;
+    num = num/10;
+    
+
+}
+return sum;
+}
+
